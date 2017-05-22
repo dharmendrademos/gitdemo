d4d51b7b1bde30b999db6852382962677e76f8c7
Dharmendra <dharmendra.ladi@gmail.com>
2017-05-22 23:04:29 +0530

    Initial commit

---
+++ GitDemoConsoleApp/GitDemoConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloGit();
+        }
+
+        private static void HelloGit()
+        {
+            Console.WriteLine("Hello Git!");
+        }
+    }
+
+}
